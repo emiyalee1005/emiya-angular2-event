608583e0a177b9d80f81822157f84f5f40a60dc9
EmiyaLee <370489964@qq.com>
2016-11-01 14:37:54 +0800

    first commit

---
+++ Event.js
@@ -0,0 +1,60 @@
+'use strict';
+var core_1 = require("@angular/core");
+var EventSyncEmitter_1 = require("./EventSyncEmitter");
+var Event = (function () {
+    function Event() {
+    }
+    Event.createEmitter = function (id, isSync) {
+        if (isSync === void 0) { isSync = true; }
+        if (isSync != true && (typeof (Event.emitters[id]) == "undefined" || Event.emitters[id] == null)) {
+            Event.emitters[id] = new core_1.EventEmitter();
+        }
+        else if (isSync == true && (typeof (Event.emittersSync[id]) == "undefined" || Event.emittersSync[id] == null)) {
+            Event.emittersSync[id] = new EventSyncEmitter_1.EventSyncEmitter();
+        }
+    };
+    Event.getEmitter = function (id, isSync) {
+        if (isSync === void 0) { isSync = true; }
+        Event.createEmitter(id, isSync);
+        if (isSync != true)
+            return Event.emitters[id];
+        else
+            return Event.emittersSync[id];
+    };
+    Event.removeEmitter = function (id, isSync) {
+        if (isSync === void 0) { isSync = true; }
+        if (isSync != true)
+            delete Event.emitters[id];
+        else
+            delete Event.emittersSync[id];
+    };
+    Event.emit = function (id, msg, isSync) {
+        if (isSync === void 0) { isSync = true; }
+        return Event.getEmitter(id, isSync).emit(msg);
+    };
+    Event.subscribe = function (id, callback, isSync) {
+        if (isSync === void 0) { isSync = true; }
+        var emitter = Event.getEmitter(id, isSync);
+        return emitter.subscribe(function (param0, param1) {
+            if (emitter instanceof core_1.EventEmitter)
+                callback(undefined, param0);
+            else
+                callback(param0, param1);
+        });
+    };
+    Event.subscribeOnce = function (id, callback, isSync) {
+        if (isSync === void 0) { isSync = true; }
+        var emitter = Event.getEmitter(id, isSync), obj = emitter.subscribe(function (param0, param1) {
+            obj.unsubscribe();
+            if (emitter instanceof core_1.EventEmitter)
+                callback(undefined, param0);
+            else
+                callback(param0, param1);
+        });
+        return obj;
+    };
+    return Event;
+}());
+Event.emitters = window['eventQueue'] ? window['eventQueue'] : (window['eventQueue '] = {});
+Event.emittersSync = window['eventSyncQueue'] ? window['eventSyncQueue'] : (window['eventSyncQueue '] = {});
+exports.Event = Event;
---
+++ EventSyncEmitter.js
@@ -0,0 +1,37 @@
+'use strict';
+var EventSyncEmitter = (function () {
+    function EventSyncEmitter() {
+        this._listener = [];
+    }
+    EventSyncEmitter.prototype.emit = function (msg) {
+        var _event = {
+            defaultPrevented: false,
+            propagationPrevented: false,
+            preventDefault: function () {
+                _event.defaultPrevented = true;
+            },
+            stopPropagation: function () {
+                _event.propagationPrevented = true;
+            }
+        };
+        for (var c in this._listener) {
+            this._listener[c](_event, msg);
+            if (_event.propagationPrevented != false)
+                break;
+        }
+        return _event;
+    };
+    EventSyncEmitter.prototype.subscribe = function (callback) {
+        var _this = this;
+        if (this._listener.indexOf(callback) < 0) {
+            this._listener.push(callback);
+        }
+        return {
+            unsubscribe: function () {
+                _this._listener.splice(_this._listener.indexOf(callback), 1);
+            }
+        };
+    };
+    return EventSyncEmitter;
+}());
+exports.EventSyncEmitter = EventSyncEmitter;
